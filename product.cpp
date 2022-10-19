da67b48ab894355a7dc6dea938580775501e4c55
kinshuk-raj <sejalm56@gmail.com>
2022-10-19 20:03:22 +0530

    second change

---
+++ product.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,b,c;
+cout<<"enter 2 numbers"<<endl;
+cin>>a>>b;
+c=a*b;
+cout<<"the product of numbers is "<<c<<endl;
+
+return 0;
+}
